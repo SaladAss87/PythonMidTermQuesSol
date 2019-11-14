28181108d16867c676ef6c5713be81cfc032b528
Zobaer Ul Islam <54277547+SaladAss87@users.noreply.github.com>
2019-11-15 01:34:47 +0600

    Add files via upload

---
+++ Python Mid Term Ques Problem 1.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#Corporate Tax Rate
+#Category List
+print("1: Publicly Traded Company")
+print("2: Non-Publicly Traded Company")
+print("3: Publicly Traded Cigarette Company")
+print("4: Mobile Phone Operator Company")
+print("5: Publicly Traded Mobile Phone Company")
+
+#Inputs
+taxpayerCategory = eval(input("Enter Your Category Number: "))
+
+#Calculating Tax Amount#
+
+# 1. Publicly Traded Company
+if taxpayerCategory == 1:
+    dividendRate = eval(input("Did The Publicly Traded Company Declare Dividend? If Yes Then Enter the Rate in %: "))
+    profitAmount = eval(input("Enter The Profit Amount: "))
+    if dividendRate == 0:
+        if profitAmount>1000000:
+            netTax = profitAmount*.275
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.255
+    elif dividendRate > 30:
+        if profitAmount>1000000:
+            netTax = profitAmount*.2475
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.2075
+    elif dividendRate < 10:
+        if profitAmount>1000000:
+            netTax = profitAmount*.35
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.30
+
+# 2. Non Publicly Traded Company
+if taxpayerCategory == 2:
+    paidupCapitalThroughIPO = eval(input("How Much Share of your Paid Up Capital was Transfered thought IPO? Enter the rate in %: "))
+    profitAmount = eval(input("Enter The Profit Amount: "))
+    if paidupCapitalThroughIPO >= 20:
+        if profitAmount>1000000:
+            taxBeforeRebate = profitAmount*.35
+            netTax = taxBeforeRebate  - taxBeforeRebate*.10
+        elif profitAmount < 1000000:
+            taxBeforeRebate = profitAmount*.32
+            netTax = taxBeforeRebate  - taxBeforeRebate*.10
+        else:
+            if profitAmount>1000000:
+                netTax = profitAmount*.35
+            elif profitAmount < 1000000:
+                netTax = profitAmount*.32
+
+# 3. Publicly Traded Cigarette Company
+if taxpayerCategory == 3:
+    dividendRate = eval(input("Did The Publicly Traded Cigarette Company Declare Dividend? If Yes Then Enter the Rate in %: "))
+    profitAmount = eval(input("Enter The Profit Amount: "))
+    if dividendRate == 0:
+        if profitAmount>1000000:
+            netTax = profitAmount*.40
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.35
+    elif dividendRate > 30:
+        if profitAmount>1000000:
+            netTax = profitAmount*.2475
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.2075
+    elif dividendRate < 10:
+        if profitAmount>1000000:
+            netTax = profitAmount*.35
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.30
+            
+# 4. Mobile Phone Operator Company
+if taxpayerCategory == 4:
+    profitAmount = eval(input("Enter The Profit Amount: "))
+    if profitAmount>1000000:
+        netTax = profitAmount*.45
+    elif profitAmount < 1000000:
+        netTax = profitAmount*.40
+
+# 5. Publicly Traded Mobile Company
+if taxpayerCategory == 5:
+    dividendRate = eval(input("Did The Publicly Traded Mobile Company Declare Dividend? If Yes Then Enter the Rate in %: "))
+    profitAmount = eval(input("Enter The Profit Amount: "))
+    if dividendRate == 0:
+        if profitAmount>1000000:
+            netTax = profitAmount*.40
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.35
+    elif dividendRate > 30:
+        if profitAmount>1000000:
+            netTax = profitAmount*.2475
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.2075
+    elif dividendRate < 10:
+        if profitAmount>1000000:
+            netTax = profitAmount*.35
+        elif profitAmount < 1000000:
+            netTax = profitAmount*.30
+            
+#Print Tax Amount            
+print("Net Tax For The Company is: ",round(netTax,2))
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
