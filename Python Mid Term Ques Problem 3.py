86bbe0d7ee1e7a6bd98bc5a2d84ee63e320e26cc
Zobaer Ul Islam <54277547+SaladAss87@users.noreply.github.com>
2019-11-15 22:53:12 +0600

    Add files via upload

---
+++ Python Mid Term Ques Problem 3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Python Mid Term Ques Problem 3
+#In Question there might be an error as the price of quantity 20 to 50 is not given
+#I took quantity 10 to 50 shipping price as 10.5 and 50+ Can't be shipped
+
+packageWeight = eval(input("Enter Your Package Weight: "))
+
+if 0 < packageWeight <= 1:
+    print("Total Shipping Cost Is: 3.5")
+elif 1 < packageWeight <= 3:
+    print("Total Shipping Cost Is: 5.5 ")
+elif 3 < packageWeight <= 10:
+    print("Total Shipping Cost Is: 8.5")
+elif 10 < packageWeight <= 50:
+    print("Total Shipping Cost Is: 10.5",totalShippingCost)
+else:
+    print("The Package Can't Be Shipped")
+
