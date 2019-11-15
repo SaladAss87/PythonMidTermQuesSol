ac9190a21ac0d408aa0bf38b6d97417a373a7b38
Zobaer Ul Islam <54277547+SaladAss87@users.noreply.github.com>
2019-11-15 23:39:46 +0600

    Add files via upload

---
+++ Python Mid Term Ques Problem 5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+monthlyPayment=eval(input ("Enter value for monthly payment such as 100.00: "))
+interestRate=eval(input ("Enter annual interest rate: "))
+savingsAmount=0
+month=1
+while month<=6:
+    j=1+(interestRate/(100*12))
+    monthlyPayment=monthlyPayment*j
+    savingsAmount=savingsAmount+monthlyPayment
+    print(round(savingsAmount,3),round(monthlyPayment,2))
+    month=month+1
+
+
+# In[ ]:
+
+
+
+
